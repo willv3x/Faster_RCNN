b1394c16971a5baf1b7aadcdf7008f150eb740c6
willv3x <willv3x@gmail.com>
2023-06-05 22:37:41 -0300

    Prosseguindo

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,6 +1,10 @@
 from torch.utils.data import DataLoader
 
 
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+
 def build_data_loader(dataset, batch_size, shuffle, num_workers, drop_last):
-    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, drop_last=drop_last,
-                      collate_fn=lambda batch: tuple(zip(*batch)))
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle,
+                      num_workers=num_workers, drop_last=drop_last, collate_fn=collate_fn)
